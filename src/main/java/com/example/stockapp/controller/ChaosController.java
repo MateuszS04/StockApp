0605dcf0f2abd8ef243e9d2ae027421c44c1eec9
MateuszS04 <mateuszsondej04@gmail.com>
2026-04-28 21:41:46 +0200

    chaos endpoint refactor for indepedency from controller

--- src/main/java/com/example/stockapp/controller/ChaosController.java
+++ src/main/java/com/example/stockapp/controller/ChaosController.java
@@ -1,6 +1,7 @@
 package com.example.stockapp.controller;
 
 
+import com.example.stockapp.service.JvmHalter;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
@@ -16,22 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChaosController {
 
     private static final Logger log = LoggerFactory.getLogger(ChaosController.class);
+    private final JvmHalter jvmHalter;
+
+    public ChaosController(JvmHalter jvmHalter) {
+        this.jvmHalter = jvmHalter;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public void chaos(){
+    public void chaos() {
         log.warn("Chaos endpoint invoked - this instance will stop in 100ms");
-
-        Thread killer = new Thread(()-> {
-            try{
-                Thread.sleep(100);
-            } catch (InterruptedException e){
-                Thread.currentThread().interrupt();
-            }
-            Runtime.getRuntime().halt(1);
-        },
-                "chaos-killer");
-        killer.setDaemon(false);
-        killer.start();
+        jvmHalter.haltAfterDelay();
     }
 }
---
+++ src/main/java/com/example/stockapp/service/JvmHalter.java
@@ -0,0 +1,22 @@
+package com.example.stockapp.service;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JvmHalter {
+
+    public void haltAfterDelay(){
+        Thread killer= new Thread(()->{
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+            Runtime.getRuntime().halt(1);
+        },
+                "Chaos-killer");
+        killer.setDaemon(false);
+        killer.start();
+    }
+}
